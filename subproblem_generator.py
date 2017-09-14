e3f992172370ae3a36c1b221f206d5aa15a5e5f3
modanesh <mohamad4danesh@gmail.com>
2017-09-14 19:14:40 +0430

    update

--- subproblem_generator.py
+++ subproblem_generator.py
@@ -24,6 +24,7 @@
 #
 #
 #     S.append()
+import re
 
 
 def subproblem_generator(ag, sct):
@@ -36,3 +37,32 @@ def subproblem_generator(ag, sct):
             if sct[i][5] == sct[j][4]:
                 s.append((sct[i][1], sct[i][2], sct[i][3], sct[i][4], sct[i][5], sct[j][1], sct[j][2], sct[j][3], sct[j][4], sct[j][5]))
 
+
+
+
+
+def camera_connections(camera_tracks):
+
+    file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/camera_correlations.txt")
+
+    correlations = []
+    subproblems = []
+
+    for line in file.readlines():
+        correlations.append(([int(s) for s in re.findall(r'\d+', line)][0], [int(s) for s in re.findall(r'\d+', line)][1]))
+
+    print(correlations)
+
+    for i in range(0, len(camera_tracks)):
+        for j in range(0, len(correlations)):
+            first_cam = correlations[j][0]
+            second_cam = correlations[j][1]
+
+            if (camera_tracks[i][0] == first_cam and camera_tracks[i][5] == second_cam) or (camera_tracks[i][0] == second_cam and camera_tracks[i][5] == first_cam):
+                subproblems.append(camera_tracks[i])
+
+
+
+
+if __name__ == '__main__':
+    camera_connections()
