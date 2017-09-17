bd890d41fd8000ed01b8a6cf943a5b229b1179ed
modanesh <mohamad4danesh@gmail.com>
2017-09-17 23:33:54 +0430

    update

--- tmp.py
+++ tmp.py
@@ -1,13 +1,11 @@
 import re
 
-file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp.txt")
+import os
 
-count = 0
-for line in file.readlines():
-    h = [int(s) for s in re.findall(r'\d+', line)][0]
-    e = [int(s) for s in re.findall(r'\d+', line)][1]
+path = "/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/resources/22/better_foreground/"
 
-    if e < 14:
-        count += 1
+for filename in os.listdir(path):
 
-print(count)
+    h = [int(s) for s in re.findall(r'\d+', filename)]
+
+    print(h)
