7fde87ba2893d03d8501e071d8c9086fd51c01a1
modanesh <mohamad4danesh@gmail.com>
2017-09-19 18:19:18 +0430

    update

--- subproblem_generator.py
+++ subproblem_generator.py
@@ -1,27 +1,6 @@
 import re
+import os
 
 
-def subproblem_generator(sct):
-    s = []
+file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/camera_regions.txt")
 
-    file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/camera_correlations.txt")
-    correlations = []
-
-    for line in file.readlines():
-        if not line.startswith("#"):
-            correlations.append(([int(s) for s in re.findall(r'\d+', line)][0], [int(s) for s in re.findall(r'\d+', line)][1], [int(s) for s in re.findall(r'\d+', line)][2], [int(s) for s in re.findall(r'\d+', line)][3]))
-
-    for i in range(0, len(sct)):
-        for j in range(0, len(sct)):
-            for k in range(0, len(correlations)):
-                if sct[i][5] == sct[j][4]:
-
-                    if sct[i][1] == correlations[k][0] and sct[i][3] == correlations[k][1] and sct[j][1] == correlations[k][2] and sct[j][3] == correlations[k][3]:
-                        s.append((0, sct[i], 1, sct[j]))
-                    elif sct[i][1] == correlations[k][2] and sct[i][3] == correlations[k][3] and sct[j][1] == correlations[k][0] and sct[j][3] == correlations[k][1]:
-                        s.append((0, sct[j], 1, sct[i]))
-
-
-if __name__ == '__main__':
-    # TODO: SCTs
-    subproblem_generator()
