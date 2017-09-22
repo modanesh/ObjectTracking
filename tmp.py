6ab2e2d318b2c02d92893a0f058659bc8877439b
modanesh <mohamad4danesh@gmail.com>
2017-09-22 09:59:38 +0330

    update

--- tmp.py
+++ tmp.py
@@ -8,8 +8,13 @@ file.close()
 
 file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp2.txt", "w")
 
-arr1 = [26,33,45,50,8]
-arr2 = []
+arr1 = [11,
+18,
+19,
+20,
+26,
+7]
+arr2 = [1]
 print(len(arr1)*len(arr2))
 
 for line in lines:
@@ -18,7 +23,7 @@ for line in lines:
     cam2 = [int(s) for s in re.findall(r'\d+', line)][2]
     id2 = [int(s) for s in re.findall(r'\d+', line)][3]
 
-    if cam1 == 2 and cam2 == 12 and id1 in arr1 and id2 in arr2:
+    if cam1 == 21 and cam2 == 11 and id1 in arr1 and id2 in arr2:
         print("eh")
         print(cam1, id1, cam2, id2)
 
