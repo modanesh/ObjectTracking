47f9d6520f986f1c9da2b8f0523bb2b5980b805f
modanesh <mohamad4danesh@gmail.com>
2017-10-08 18:04:46 +0330

    update

--- tmp.py
+++ tmp.py
@@ -1,20 +1,38 @@
 import re
 
-file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp2.txt")
+file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp.txt")
+file2 = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp2.txt", "w")
 
-a = []
-b = [1]
-c = [6]
+# a_2_4_in = [21,
+# 26,
+# 27,
+# 33,
+# 45,
+# 46,
+# 50,
+# 51,
+# 8]
 
-file2 = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp.txt", "w")
+# a_2_4_out = [14,
+# 1,
+# 20,
+# 24,
+# 28,
+# 2,
+# 31,
+# 36,
+# 37,
+# 6]
+
+b = []
 
 for line in file.readlines():
-    if [int(s) for s in re.findall(r'\d+', line)][0] == 17 and [int(s) for s in re.findall(r'\d+', line)][1] in a:
-        if [int(s) for s in re.findall(r'\d+', line)][2] == 18 and [int(s) for s in re.findall(r'\d+', line)][3] in b:
-            print("")
-        # elif [int(s) for s in re.findall(r'\d+', line)][2] == 18 and [int(s) for s in re.findall(r'\d+', line)][3] in c:
-        #     print("")
-        else:
+    cam_1 = [int(s) for s in re.findall(r'\d+', line)][0]
+    id_1 = [int(s) for s in re.findall(r'\d+', line)][1]
+    cam_2 = [int(s) for s in re.findall(r'\d+', line)][2]
+    id_2 = [int(s) for s in re.findall(r'\d+', line)][3]
+    if cam_1 == 2 and cam_2 == 13:
+        if id_1 in a and id_2 in b:
             file2.write(line)
     else:
         file2.write(line)
