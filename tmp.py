0240fe062143490ea95e06d2083d5bf1d3f52972
modanesh <mohamad4danesh@gmail.com>
2017-10-08 18:30:40 +0330

    update

--- tmp.py
+++ tmp.py
@@ -3,36 +3,18 @@ import re
 file = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp.txt")
 file2 = open("/Users/Mohamad/Desktop/MulticameraObjectDetection/OurCode/ObjectTracking/tmp2.txt", "w")
 
-# a_2_4_in = [21,
-# 26,
-# 27,
-# 33,
-# 45,
-# 46,
-# 50,
-# 51,
-# 8]
-
-# a_2_4_out = [14,
-# 1,
-# 20,
-# 24,
-# 28,
-# 2,
-# 31,
-# 36,
-# 37,
-# 6]
-
-b = []
+in_12_1 = [10,11,15,16,18,19,1,21,22,23,24,25,26,2,6,7,8,9]
+out_12_1 = [10,11,12,13,14,16,17,18,19,1,20,21,22,23,24,25,2,3,4,6,7,8,9]
+in_13_1 = [10,11,13,14,17,18,19,1,20,21,22,24,25,26,2,30,31,32,33,34,35,3,4,5,8,9]
+out_13_1 = [10,15,16,18,21,22,23,25,27,28,29,32,7,8,9]
 
 for line in file.readlines():
     cam_1 = [int(s) for s in re.findall(r'\d+', line)][0]
     id_1 = [int(s) for s in re.findall(r'\d+', line)][1]
     cam_2 = [int(s) for s in re.findall(r'\d+', line)][2]
     id_2 = [int(s) for s in re.findall(r'\d+', line)][3]
-    if cam_1 == 2 and cam_2 == 13:
-        if id_1 in a and id_2 in b:
+    if cam_1 == 12 and cam_2 == 13:
+        if id_1 in out_12_1 and id_2 in in_13_1:
             file2.write(line)
     else:
         file2.write(line)
